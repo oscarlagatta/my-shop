ff1a7e149a5e7f2863e27774058a2541d4c45a2c
Oscar <oscarlagatta@gmail.com>
2023-04-19 15:34:54 +0100

    CMS & API with Pocket Base

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,9 +1,37 @@
+import {Product} from "@/model/product";
+import {useEffect, useState} from "react";
+import {pb} from "../../pocketbase";
+
 export function ShopPage() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(()=> {
+        loadData();
+    }, []);
+
+    function loadData() {
+        pb.collection('products').getList<Product>()
+            .then(res => {
+                setProducts(res.items)
+            });
+    }
     return (
         <div>
             <h1 className="title">SHOP</h1>
 
+
+            {
+                products.map( product => {
+                    return (
+                        <li key={product.id}>
+                            {product.name}
+                        </li>
+                    )
+                })
+            }
             content here...
+            <button className="btn" onClick={loadData}>Load Data</button>
         </div>
     )
 }
